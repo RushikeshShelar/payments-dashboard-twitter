f57935b319d6d6d1f4b1e39707a3d1c8f4300d0f
Rushi <rushikeshshelar.cs@gmail.com>
2024-01-09 02:54:12 +0530

    color Changes

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -9,7 +9,7 @@ const DashboardLayout = ({
 }) => {
     return ( 
         <div className="h-full relative">
-            <div className="hidden h-full md:flex md:w-[16rem] md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+            <div className="hidden h-full md:flex md:w-[16rem] md:flex-col md:fixed md:inset-y-0 z-[80] bg-[#1e2640]">
                 <Sidebar />
             </div>
             <main className="md:pl-[16rem]">
